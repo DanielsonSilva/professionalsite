6bea50a226cd5dac5e381e1a69128adf28640a6c
Danielson Flávio Xavier da Silva <danielson_fx@yahoo.com.br>
2020-05-12 16:50:15 -0300

    Use the DIRECTORY SEPARATOR

--- applications/diceroller/app/Controllers/DiceRollerController.php
+++ applications/diceroller/app/Controllers/DiceRollerController.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Controllers;
 
-require_once(__DIR__ . '/../../../../vendor/autoload.php');
+require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..'
+    . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
 use CodeIgniter\HTTP\RequestInterface;
 use phpDocumentor\Reflection\Types\Boolean;
